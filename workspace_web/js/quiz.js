c21e13fcf256255980a4289a881a59216171e341
minhyeok <111694761+Minhyeok51@users.noreply.github.com>
2022-08-31 17:30:46 +0900

    Add files via upload

---
+++ workspace_web/js/quiz.js
@@ -0,0 +1,14 @@
+let strUrl1 = "https://human.or.kr";
+let strUrl2 = "http://human.or.kr";
+let strUrl3 = "http.human.or.kr";
+
+const changeLink = (input) => {
+  for(let url1 in input){
+    
+  }
+}
+  
+
+console.log(changeLink(strUrl1));
+console.log(changeLink(strUrl2));
+console.log(changeLink(strUrl3));
